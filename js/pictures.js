e66663831b5c3cff7e98cc4a1e61b44ed1418f6c
edward29 <caacbayedward@gmail.com>
2022-05-22 12:01:20 +0800

    final commit

--- js/pictures.js
+++ js/pictures.js
@@ -161,28 +161,28 @@ function onWndLoad() {
 
 }
 
-// document.addEventListener("contextmenu", (e) => {
-//     e.preventDefault();
-// });
+document.addEventListener("contextmenu", (e) => {
+    e.preventDefault();
+});
 
-// document.onkeydown = (e) => {
-//     if (event.keyCode == 123) {
-//         return false;
-//     }
+document.onkeydown = (e) => {
+    if (event.keyCode == 123) {
+        return false;
+    }
 
-//     if (e.ctrlKey && e.shiftKey && e.keyCode === "I".charCodeAt(0)) {
-//         return false;  
-//     }
-//     if (e.ctrlKey && e.shiftKey && e.keyCode === "C".charCodeAt(0)) {
-//         return false;  
-//     }
-//     if (e.ctrlKey && e.shiftKey && e.keyCode === "J".charCodeAt(0)) {
-//         return false;  
-//     }
-//     if (e.ctrlKey && e.shiftKey && e.keyCode === "U".charCodeAt(0)) {
-//         return false;  
-//     }
-//     if (e.ctrlKey && e.keyCode == "U".charCodeAt(0)) {
-//         return false;  
-//     }
-// }
+    if (e.ctrlKey && e.shiftKey && e.keyCode === "I".charCodeAt(0)) {
+        return false;  
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode === "C".charCodeAt(0)) {
+        return false;  
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode === "J".charCodeAt(0)) {
+        return false;  
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode === "U".charCodeAt(0)) {
+        return false;  
+    }
+    if (e.ctrlKey && e.keyCode == "U".charCodeAt(0)) {
+        return false;  
+    }
+}
